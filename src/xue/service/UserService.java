566618baf1646f7dc6eb90220d84dfd1e7c3f712
xueshun <xueshun1211@163.com>
2017-06-09 11:56:21 +0800

    dubbox provier rest

---
+++ src/xue/service/UserService.java
@@ -0,0 +1,47 @@
+package xue.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+
+import xue.entity.User;
+@Path("/userService")
+/*Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+ *  指定registerUser()接收JSON格式的数据，Rest框架会自动将json数据反序列化为User对象
+ */
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+/*
+ * @Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+ * 指定getUser()输出JSON格式的数据。框架会自动将User对象序列化为JSON数据
+ */
+@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+public interface UserService {
+	@GET
+    @Path("/testget")
+	public void testget();
+    @GET
+    @Path("/getUser")
+	public User getUser();
+	@GET
+	@Path("/get/{id : \\d+}")
+	public User getUser(@PathParam(value = "id") Integer id);
+	@GET
+	@Path("/get/{id : \\d+}/{name : [a-zA-Z][0-9]}")
+	public User getUser(@PathParam(value = "id") Integer id, @PathParam(value = "name") String name);
+    @POST
+    @Path("/testpost")
+	public void testpost();
+    @POST
+    @Path("/postUser")
+	public User postUser(User user);
+	@POST
+	@Path("/post/{id}")
+	public User postUser(@PathParam(value = "id") String id);
+
+}
